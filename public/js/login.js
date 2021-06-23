7ed0acaad12604a61237c54cee3bca28f0cec5bd
michaelshxw <michaelshxw@gmail.com>
2021-06-23 21:02:18 +0800

    login and sign up form handlers, same as activity 20 as it was done in class

--- public/js/login.js
+++ public/js/login.js
@@ -1,7 +1,58 @@
 // login form handler
 
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (email && password) {
+        const response = await fetch('/routes/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert('Failed to log in.');
+        }
+    }
+};
+
 // login event listener
+
+document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
 
 // signup form handler
 
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (username && email && password) {
+        const response = await fetch('/routes/users', {
+            method: 'POST',
+            body: JSON.stringify({ username, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert('Failed to sign up.');
+        }
+    }
+};
+
 // signup event listener
+
+document
+    .querySelector('.signup-form')
+    .addEventListener('submit', signupFormHandler);
--- public/js/logout.js
+++ public/js/logout.js
@@ -1,3 +1,18 @@
 // logout event handler
 
+const logout = async () => {
+    const response = await fetch('/routes/users/logout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert('Failed to log out.');
+    }
+};
+
 // logout event listener
+
+document.querySelector('#logout').addEventListener('click', logout);
