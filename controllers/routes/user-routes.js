aa8bbc4c3886de8b9d8003296a329f8052f7f610
michaelshxw <michaelshxw@gmail.com>
2021-07-07 13:30:15 +0800

    add login post route to check username and password

--- controllers/routes/user-routes.js
+++ controllers/routes/user-routes.js
@@ -20,3 +20,42 @@ router.post('/', async (req, res) => {
         res.status(500).json(error);
     }
 });
+
+// login 
+
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({
+            where: {
+                email: req.body.email,
+            },
+        });
+
+        if (!userData) {
+            res.status(400).json({
+                message: "Incorrect email or password. Please try again."
+            });
+            return;
+        }
+
+        const passwordIsValid = await userData.checkPassword(req.body.password);
+
+        if (!passwordIsValid) {
+            res.status(400).json({
+                message: "Incorrect email or password. Please try again."
+            })
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.isLoggedIn = true;
+            res.status(200).json({
+                user: userData, message: "You have succesfully logged in"
+            });
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json(error);
+    }
+});
+
